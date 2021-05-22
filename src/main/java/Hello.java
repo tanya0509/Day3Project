708d1ee3ef1dd27787190316f77252cf2d0681d4
Tanya <tanya.stamboliyska@yahoo.com>
2021-05-22 10:07:28 -0500

    added something update

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("more changes");
+        System.out.println(" local git hub repo");
+
     }
 }
